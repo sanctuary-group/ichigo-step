a9e0ebd701e73ba2724ffb66be69968e92a01856
ryuimoto <narrow617@gmail.com>
2026-05-28 13:16:24 +0900

    1:1チャット メッセージボックス フロント修正

--- apps/api/resources/js/Layouts/DashboardLayout.tsx
+++ apps/api/resources/js/Layouts/DashboardLayout.tsx
@@ -3,11 +3,11 @@ import { Header } from "@/components/layout/header";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex min-h-screen min-h-0 bg-background">
+        <div className="flex h-screen bg-background overflow-hidden">
             <Sidebar />
-            <div className="flex flex-col flex-1 min-w-0 min-h-screen">
+            <div className="flex flex-col flex-1 min-w-0 min-h-0">
                 <Header />
-                <main className="flex-1 min-h-0 flex flex-col">{children}</main>
+                <main className="flex-1 min-h-0 flex flex-col overflow-hidden">{children}</main>
             </div>
         </div>
     );
